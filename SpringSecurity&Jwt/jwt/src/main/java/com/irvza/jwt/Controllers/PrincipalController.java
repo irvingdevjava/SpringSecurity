ad662424f8ceb5a366e51c67061d145d5d338d12
irvingdevjava <irvingspringboot@gmail.com>
2024-07-25 13:39:17 -0600

    Funcionalidad eliminar usuario a partir del id

--- SpringSecurity&Jwt/jwt/src/main/java/com/irvza/jwt/Controllers/PrincipalController.java
+++ SpringSecurity&Jwt/jwt/src/main/java/com/irvza/jwt/Controllers/PrincipalController.java
@@ -6,8 +6,10 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.irvza.jwt.Controllers.request.CreateUserDTO;
@@ -50,6 +52,13 @@ public class PrincipalController {
 
     }
 
+
+    @DeleteMapping("/user")
+    public String deteleUser(@RequestParam String id){
+            userRepository.deleteById(Long.parseLong(id));
+
+        return "Usuario eliminado con el id:".concat(id);
+    }
 
     
     
