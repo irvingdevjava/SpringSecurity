1fbec020d8cf490aa09adf646ee4f0aa6548145b
irvingdevjava <irvingspringboot@gmail.com>
2024-07-25 10:06:34 -0600

    Modelo de roles

---
+++ SpringSecurity&Jwt/jwt/src/main/java/com/irvza/jwt/Modelos/ERole.java
@@ -0,0 +1,8 @@
+package com.irvza.jwt.Modelos;
+
+public enum ERole {
+    ADMIN,
+    USER,
+    INVITED
+
+}
---
+++ SpringSecurity&Jwt/jwt/src/main/java/com/irvza/jwt/Modelos/RoleEntity.java
@@ -0,0 +1,32 @@
+package com.irvza.jwt.Modelos;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@Entity
+@Table(name="roles")
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private ERole role;
+
+}
