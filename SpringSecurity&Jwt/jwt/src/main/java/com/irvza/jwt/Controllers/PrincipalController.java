8860e7fac437963114ce6284b92bcc022657fdd7
irvingdevjava <irvingspringboot@gmail.com>
2024-07-25 13:38:32 -0600

    Controlador con funcinalidad de crear usuario.

---
+++ SpringSecurity&Jwt/jwt/src/main/java/com/irvza/jwt/Controllers/PrincipalController.java
@@ -0,0 +1,57 @@
+package com.irvza.jwt.Controllers;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.irvza.jwt.Controllers.request.CreateUserDTO;
+import com.irvza.jwt.Modelos.ERole;
+import com.irvza.jwt.Modelos.RoleEntity;
+import com.irvza.jwt.Modelos.UserEntity;
+import com.irvza.jwt.Repositories.UserRepository;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.validation.Valid;
+
+
+@RestController
+@RequestMapping("/api/v1")
+public class PrincipalController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    public ResponseEntity<?> createUser(@Valid @RequestBody CreateUserDTO createUserDTO){
+
+        Set<RoleEntity> roles=createUserDTO.getRoles().stream()
+            .map(role -> RoleEntity.builder()
+                    .name(ERole.valueOf(role))
+                    .build())
+            .collect(Collectors.toSet());
+
+        UserEntity userEntity=UserEntity.builder()
+                                .username(createUserDTO.getUsername())
+                                .password(createUserDTO.getPassword())
+                                .email(createUserDTO.getEmail())
+                                .roles(roles)
+                                .build();
+                                
+
+        userRepository.save(userEntity);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userEntity);
+
+    }
+
+
+    
+    
+
+}
