65aa8c3001860d54d7877c60056336a231804b00
irvingdevjava <irvingspringboot@gmail.com>
2024-07-29 15:41:20 -0600

    Creación de filtro de autenticación JWT extendido de UsernamePasswordAuthenticationFilter.

---
+++ SpringSecurity&Jwt/jwt/src/main/java/com/irvza/jwt/Conguration/Security/Filters/JwtAuthenticationFIlter.java
@@ -0,0 +1,7 @@
+package com.irvza.jwt.Conguration.Security.Filters;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JwtAuthenticationFIlter extends UsernamePasswordAuthenticationFilter{
+
+}
