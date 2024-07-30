7d57679070b15f7a77c995f10aacaceda1b4c228
irvingdevjava <irvingspringboot@gmail.com>
2024-07-29 21:35:00 -0600

    Refactorización de configuración de seguridad: integración del filtro de autenticación JWT y ajuste en la configuración del AuthenticationManager para usar UserDetailServiceImpl.

--- SpringSecurity&Jwt/jwt/src/main/java/com/irvza/jwt/Conguration/Security/SecurityConfig.java
+++ SpringSecurity&Jwt/jwt/src/main/java/com/irvza/jwt/Conguration/Security/SecurityConfig.java
@@ -1,25 +1,36 @@
 package com.irvza.jwt.Conguration.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.irvza.jwt.Conguration.Security.Filters.JwtAuthenticationFilter;
+import com.irvza.jwt.Conguration.Security.Service.UserDetailServiceImpl;
+import com.irvza.jwt.Conguration.Security.jwt.JwtUtils;
+
 @Configuration
-// @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    UserDetailServiceImpl userDetailServiceImpl;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, AuthenticationManager authenticationManager ) throws Exception{
+
+        JwtAuthenticationFilter jwtAuthenticationFilter= new JwtAuthenticationFilter(jwtUtils);
+        jwtAuthenticationFilter.setAuthenticationManager(authenticationManager);
+        jwtAuthenticationFilter.setFilterProcessesUrl("/api/v1/login"); //Ruta de login
+
         return httpSecurity
             .csrf(config->config.disable())
             .authorizeHttpRequests(auth -> {
@@ -29,19 +40,8 @@ public class SecurityConfig {
             .sessionManagement(session -> {
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
             })
-            .httpBasic(Customizer.withDefaults())
+            .addFilter(jwtAuthenticationFilter)
             .build();
-    }
-
-    @Bean
-    UserDetailsService userDetailsService(){
-        InMemoryUserDetailsManager manager= new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("Santiago")
-            .password("1234")
-            .roles("ADMIN")
-            .build()
-        );
-        return manager;
     }
 
     @Bean
@@ -56,11 +56,12 @@ public class SecurityConfig {
     AuthenticationManager authenticationManager(HttpSecurity httpSecurity, PasswordEncoder passwordEncoder) throws Exception{
 
         return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
-                    .userDetailsService(userDetailsService())
+                    .userDetailsService(userDetailServiceImpl)
                     .passwordEncoder(passwordEncoder)
                     .and()
                     .build();
 
     }
+
 
 }
