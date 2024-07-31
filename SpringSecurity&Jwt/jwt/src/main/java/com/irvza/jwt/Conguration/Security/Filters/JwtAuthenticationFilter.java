a75dc7db23ef487419828249dc119878f3f00f8c
irvingdevjava <irvingspringboot@gmail.com>
2024-07-30 22:33:20 -0600

    Refactorización de `JwtAuthenticationFilter`: Cambio en la creación del `AuthenticationManager` y ajuste de la URL de procesamiento de login. Ahora se usa `Bearer` para el token en el header `Authorization`.

--- SpringSecurity&Jwt/jwt/src/main/java/com/irvza/jwt/Conguration/Security/Filters/JwtAuthenticationFilter.java
+++ SpringSecurity&Jwt/jwt/src/main/java/com/irvza/jwt/Conguration/Security/Filters/JwtAuthenticationFilter.java
@@ -6,15 +6,16 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.irvza.jwt.Conguration.Security.jwt.JwtUtils;
 
@@ -23,25 +24,26 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    @Autowired
-    JwtUtils jwtUtils;
+    private final JwtUtils jwtUtils;
+    private final AuthenticationManager authenticationManager;
 
-    public JwtAuthenticationFilter(JwtUtils jwtUtils){
-        this.jwtUtils=jwtUtils;
+    public JwtAuthenticationFilter(JwtUtils jwtUtils, AuthenticationManager authenticationManager) {
+        this.jwtUtils = jwtUtils;
+        this.authenticationManager = authenticationManager;
+        setFilterProcessesUrl("/login"); // Configura la URL de login si es necesario
     }
 
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, 
-                                                HttpServletResponse response)
-            throws AuthenticationException {
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
         String username = "";
         String password = "";
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream()))) {
             ObjectMapper objectMapper = new ObjectMapper();
-            Map<String, String> requestBody = objectMapper.readValue(reader, Map.class);
+            Map<String, String> requestBody = objectMapper.readValue(reader,  new TypeReference<Map<String, String>>() {});
 
             username = requestBody.get("username");
             password = requestBody.get("password");
@@ -50,30 +52,26 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         }
 
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
-        return getAuthenticationManager().authenticate(authenticationToken);
+        return authenticationManager.authenticate(authenticationToken);
     }
 
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, 
+    protected void successfulAuthentication(HttpServletRequest request,
                                             HttpServletResponse response, FilterChain chain,
-            Authentication authResult) throws IOException, ServletException {
+                                            Authentication authResult) throws IOException, ServletException {
         User user = (User) authResult.getPrincipal();
         String token = jwtUtils.GenerateAccessToken(user.getUsername());
 
-        response.addHeader("Authorization", token);
+        response.addHeader("Authorization", "Bearer " + token);
 
         Map<String, Object> httpResponse = new HashMap<>();
         httpResponse.put("token", token);
-        httpResponse.put("Message", "Authenticacion correcta");
+        httpResponse.put("Message", "Autenticación correcta");
         httpResponse.put("Username", user.getUsername());
 
-        response.getWriter().write(new ObjectMapper().writeValueAsString(httpResponse));
         response.setStatus(HttpStatus.OK.value());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(new ObjectMapper().writeValueAsString(httpResponse));
         response.getWriter().flush();
-
-        super.successfulAuthentication(request, response, chain, authResult);
     }
-
-
 }
