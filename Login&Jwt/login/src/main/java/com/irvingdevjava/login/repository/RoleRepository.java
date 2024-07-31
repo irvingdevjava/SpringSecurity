fc7ff22c56065d2277bb4e86a3d53db9223ee9a3
irvingdevjava <irvingspringboot@gmail.com>
2024-07-31 15:44:04 -0600

    Implementé RoleRepository para manejar operaciones de Role
    - Añadí la interfaz RoleRepository extendiendo JpaRepository
    - Implementé el método findByName para buscar Role por nombre

--- Login&Jwt/login/src/main/java/com/irvingdevjava/login/repository/RoleRepository.java
+++ Login&Jwt/login/src/main/java/com/irvingdevjava/login/repository/RoleRepository.java
@@ -1,5 +1,15 @@
 package com.irvingdevjava.login.repository;
 
-public class RoleRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.irvingdevjava.login.entity.ERole;
+import com.irvingdevjava.login.entity.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
 }
+
